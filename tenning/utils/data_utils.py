add13ff191b5ff67bbc0282b27a5c3622b1a6bb3
Guilherme Henrique dos Santos <dos_santos_98@hotmail.com>
2022-02-05 13:50:31 -0300

    Created a method that unifies the generation of the dataset iterators, removing the need of a isolated method for each dataset type. Moreover, the documentation was improved and the attributes of the class IteratorBuilder are now public.

--- tenning/utils/data_utils.py
+++ tenning/utils/data_utils.py
@@ -1,16 +1,11 @@
-from itertools import compress
-from random import shuffle
-from random import sample
+import itertools
+import time
 from typing import Union
 from scipy import sparse
 import tensorflow as tf
 import pandas as pd
 import healpy as hp
 import numpy as np
-import itertools
-import time
-import os
-from .generic_utils import deprecated
 from .generic_utils import eye_like
 
 
@@ -89,10 +84,10 @@ class IteratorBuilder:
         two methods that must be implemented by the user: 'yielder' and 'post_process'.
         The former can handle dataset manipulation, but the user must have in mind that
         this function will store its return value in memory or disk. Thus, one needs to
-        avoid loading bigger files such as images if one wants to save memory. Is advisable
+        avoid loading bigger files such as images if one wants to save memory. It's advisable
         to leave the hard work for the 'post_process' method to save memory.
 
-        For example, when dealing with an image dataset is advisable to return the image file
+        For example, when dealing with an image dataset it's advisable to return the image file
         paths in the 'yielder' method and only load the image on the 'post_process' method.
         For this case, it will be an increase in the dataset pipeline but there won't be a
         memory overflow.
@@ -105,38 +100,39 @@ class IteratorBuilder:
             the same size of the other batches it can be dropped by setting 'drop_remainder'
             to True (defaults to True). The dataset gets shuffled each time the iterator is
             restarted if 'shuffle' is set to true (defaults to False). The 'val_ratio' and
-            the 'test_ratio' parameters specify the dataset split ratio.
+            the 'test_ratio' parameters specify the dataset split ratio (defaults to 0.2 and
+            0.3 respectively).
     """
 
     def __init__(self, **kwargs):
 
-        self._batch_size = kwargs.pop('batch_size', 32)
-        self._shuffle = kwargs.pop('shuffle', False)
-        self._drop_remainder = kwargs.pop('drop_remainder', True)
-        self._val_ratio = kwargs.pop('val_ratio', 0.2)
-        self._test_ratio = kwargs.pop('test_ratio', 0.3)
+        self.batch_size = kwargs.pop('batch_size', 32)
+        self.shuffle = kwargs.pop('shuffle', False)
+        self.drop_remainder = kwargs.pop('drop_remainder', True)
+        self.val_ratio = kwargs.pop('val_ratio', 0.2)
+        self.test_ratio = kwargs.pop('test_ratio', 0.3)
 
         if kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
-    def set_dataset(self, dataset: Union[pd.DataFrame, np.array]) -> None:
+    def set_dataset(self, dataset: Union[pd.DataFrame, np.ndarray]) -> None:
         """ Splits the dataset into train, test and validation datasets and leave them
             prepared so a iterator can be created.
 
             Args:
-                dataset: Input dataset.
+                dataset (pd.DataFrame, np.ndarray): Input dataset.
         """
 
         train_dataset, val_dataset, test_dataset = split_dataset(dataset,
-                                                                 val_ratio=self._val_ratio,
-                                                                 test_ratio=self._test_ratio)
+                                                                 val_ratio=self.val_ratio,
+                                                                 test_ratio=self.test_ratio)
 
-        self.train_dataset = train_dataset
-        self.val_dataset = val_dataset
-        self.test_dataset = test_dataset
+        self.train_dataset = self.dataset_to_tensor(train_dataset)
+        self.val_dataset = self.dataset_to_tensor(val_dataset)
+        self.test_dataset = self.dataset_to_tensor(test_dataset)
 
-    def yielder(self, data) -> Union[list, tuple]:
+    def yielder(self, data: tf.Tensor) -> Union[list, tuple]:
         """ This method must be implemented by the user and can perform any
             transformation to the dataset. The return of this method will be
             cached to memory or disk.
@@ -144,7 +140,7 @@ class IteratorBuilder:
             * It's advisable to leave the hard work to the 'post_process' method.
 
             Args:
-                params: A Tensor containing a batch from the dataset.
+                data (tf.Tensor): A Tensor containing a batch from the dataset.
 
             Returns:
                 The return value must be a iterable containing the data. For example:
@@ -165,19 +161,45 @@ class IteratorBuilder:
     def post_process(self, *args):
         raise NotImplementedError()
 
-    def gen_iterator(self, dataset, dataset_size, buffer_size=None, cache_file="", seed=42):
+    def gen_iterator(self,
+                     dataset: tf.Tensor,
+                     buffer_size: int = None,
+                     cache_file: str = "",
+                     seed: int = 42) -> tf.data.Dataset:
+        """ Creates an iterator object that will load batches of the dataset at
+            each iteration.
 
-        if (dataset_size < self._batch_size) or (dataset is None):
+            Args:
+                dataset (tf.Tensor): A tensor containing the dataset.
+                buffer_size (int, optional): A multiplication factor that will define the size of
+                                             the dataset buffer. This buffer loads more data in advance
+                                             to speed up the pipeline, for example, if the buffer size
+                                             is equal to 3, then the iterator will load a slice of the
+                                             dataset corresponding to 3*(batch_size). Defaults to None.
+                cache_file (str, optional): A file path that specifies where the dataset will be cached.
+                                            Defaults to "".
+                seed (int, optional): When the attribute shuffle is set to True this integer value will
+                                      serve as a seed value for the shuffle method. Defaults to 42.
+
+            Returns:
+                The return value will be a subclass of the tf.data.Dataset class, responsible for the dataset
+                iteration.
+
+        """
+
+        dataset_size = len(dataset)
+
+        if (dataset_size < self.batch_size) or (dataset is None):
             return None
 
         buffer_size = buffer_size or dataset_size * 3
 
         dataset = tf.data.Dataset.from_tensor_slices(dataset)
 
-        if self._shuffle:
+        if self.shuffle:
             dataset = dataset.shuffle(buffer_size=buffer_size, seed=seed)
 
-        dataset = dataset.batch(self._batch_size, drop_remainder=self._drop_remainder)
+        dataset = dataset.batch(self.batch_size, drop_remainder=self.drop_remainder)
 
         # Preprocessing of the dataset before cache. Must be a cheaper funcionality and that
         # doesn't generate bigger files to avoid memory hunger computation
@@ -194,69 +216,90 @@ class IteratorBuilder:
 
         return dataset
 
-    def train_iterator(self, buffer_size=None, cache_file="", seed=42):
-        return self.gen_iterator(self._train_dataset, len(self.train_dataset), buffer_size=buffer_size, cache_file=cache_file, seed=seed)
+    def get_iterator(self,
+                     dataset: str = 'train',
+                     buffer_size: int = None,
+                     cache_file: str = "",
+                     seed: int = 42) -> tf.data.Dataset:
+        """ Returns a iterator for a dataset.
 
-    def test_iterator(self, buffer_size=None, cache_file="", seed=42):
-        return self.gen_iterator(self._test_dataset, len(self.test_dataset), buffer_size=buffer_size, cache_file=cache_file, seed=seed)
+        Args:
+            dataset (str, optional): Which dataset will be iterated. It can be one of the following:
+                                     'train', 'test', 'val' or 'all'. The last one generates a iterator
+                                     for the whole dataset. Defaults to 'train'.
+            buffer_size (int, optional): A multiplication factor that will define the size of
+                                         the dataset buffer. This buffer loads more data in advance
+                                         to speed up the pipeline, for example, if the buffer size
+                                         is equal to 3, then the iterator will load a slice of the
+                                         dataset corresponding to 3*(batch_size). Defaults to None.
+            cache_file (str, optional): A file path that specifies where the dataset will be cached.
+                                        Defaults to "".
+            seed (int, optional): When the attribute shuffle is set to True this integer value will
+                                  serve as a seed value for the shuffle method. Defaults to 42.
 
-    def val_iterator(self, buffer_size=None, cache_file="", seed=42):
-        return self.gen_iterator(self._val_dataset, len(self.val_dataset), buffer_size=buffer_size, cache_file=cache_file, seed=seed)
+        Returns:
+            The return value will be a subclass of the tf.data.Dataset class, responsible for the dataset
+            iteration.
+        """
 
-    def num_batches(self, data_size):
-        """Returns number of batches"""
+        if dataset == 'train':
+            return self.gen_iterator(self.train_dataset, buffer_size=buffer_size, cache_file=cache_file, seed=seed)
+        elif dataset == 'test':
+            return self.gen_iterator(self.test_dataset, buffer_size=buffer_size, cache_file=cache_file, seed=seed)
+        elif dataset == 'val':
+            return self.gen_iterator(self.val_dataset, buffer_size=buffer_size, cache_file=cache_file, seed=seed)
+        else:
+            return self.gen_iterator(self.all_dataset, buffer_size=buffer_size, cache_file=cache_file, seed=seed)
 
-        if self._drop_remainder:
+    def num_batches(self, data_size: int) -> int:
+        """ Returns the number of batches in the dataset depending on
+            the batch_size value
+
+            Args:
+                data_size (int): The size of the dataset.
+
+            Returns:
+                The total number of batches of a dataset. The calculation
+                considers that all batches have the same amount of data,
+                that is, if drop_remainder is set to False the total number
+                of batches will be evaluated as if the drop_remainder was set
+                to True.
+        """
+
+        if self.drop_remainder:
             # Ignores the last batch if it is smaller than the batch size
-            return np.floor(data_size / self._batch_size).astype(np.int32)
+            return np.floor(data_size / self.batch_size).astype(np.int32)
 
-        return np.ceil(data_size / self._batch_size).astype(np.int32)
-
-    @property
-    def train_dataset(self):
-        return self._train_dataset
-
-    @train_dataset.setter
-    def train_dataset(self, train_dataset):
-
-        if isinstance(train_dataset, pd.DataFrame):
-            self._train_dataset = dataframe_to_tensor(train_dataset, train_dataset.columns)
-
-        elif isinstance(train_dataset, np.ndarray):
-            self._train_dataset = train_dataset
+        return np.ceil(data_size / self.batch_size).astype(np.int32)
 
     @property
-    def test_dataset(self):
-        return self._test_dataset
+    def all_dataset(self) -> tf.Tensor:
+        return tf.concat([self.train_dataset, self.val_dataset, self.test_dataset], axis=0)
 
-    @test_dataset.setter
-    def test_dataset(self, test_dataset):
+    def dataset_to_tensor(self, dataset: Union[pd.DataFrame, np.ndarray]) -> tf.Tensor:
 
-        if isinstance(test_dataset, pd.DataFrame):
-            self._test_dataset = dataframe_to_tensor(test_dataset, test_dataset.columns)
+        if isinstance(dataset, pd.DataFrame):
+            return dataframe_to_tensor(dataset, dataset.columns)
 
-        elif isinstance(test_dataset, np.ndarray):
-            self._test_dataset = test_dataset
+        elif isinstance(dataset, np.ndarray):
+            return tf.convert_to_tensor(dataset)
 
-    @property
-    def val_dataset(self):
-        return self._val_dataset
+    def __len__(self) -> int:
+        """ Returns the total dataset size.
 
-    @val_dataset.setter
-    def val_dataset(self, val_dataset):
+            Returns:
+                The length of the entire dataset as a integer value.
+        """
 
-        if isinstance(val_dataset, pd.DataFrame):
-            self._val_dataset = dataframe_to_tensor(val_dataset, val_dataset.columns)
+        return len(self.all_dataset)
 
-        elif isinstance(val_dataset, np.ndarray):
-            self._val_dataset = val_dataset
+    def get_config(self) -> dict:
+        """ Returns some informations about the datasets such as, their size,
+            the number of batches and the batch size.
 
-    def __len__(self):
-        """Returns the total dataset size"""
-
-        return len(self.train_dataset) + len(self.val_dataset) + len(self.test_dataset)
-
-    def get_config(self):
+            Returns:
+                A dictionary containing the metadata about the datasets.
+        """
 
         train_info = {"size": len(self.train_dataset), "num_batches": self.num_batches(len(self.train_dataset))}
         test_info = {"size": len(self.test_dataset), "num_batches": self.num_batches(len(self.test_dataset))}
@@ -265,7 +308,7 @@ class IteratorBuilder:
         config = {'train_info': train_info,
                   'val_info': val_info,
                   'test_info': test_info,
-                  'batch_size': self._batch_size}
+                  'batch_size': self.batch_size}
 
         return config
 
@@ -276,11 +319,10 @@ def dataframe_to_tensor(dataframe, column_names):
         return None
 
     # Tensors don't accept different data types, so the entire dataframe
-    # is converted to string and then transformed to tensor.
+    # is converted into string and then transformed into a tensor.
     # When manipulating the data you should convert to the right data type
     # by yourself
-    dataframe = dataframe.astype('str')  # << NEEDS AJUSTMENT BECAUSE THE RESULTING DATAFRAME
-    # IS NOT OF STRING TYPE BUT OBJECT TYPE
+    dataframe = dataframe.astype('str')
 
     tensors = [tf.convert_to_tensor(dataframe[col].values) for col in column_names]
 
@@ -314,8 +356,21 @@ def to_abspath(tensor, root_directory):
     return tf.map_fn(lambda relative: tf.strings.join([root_directory, relative], separator="/"), tensor, dtype=tf.string, swap_memory=True)
 
 
-def split_dataset(dataset, val_ratio=0.2, test_ratio=0.2):
-    """Splits dataset into train, validation and test datasets"""
+def split_dataset(dataset: Union[pd.DataFrame, np.ndarray],
+                  val_ratio: float = 0.2,
+                  test_ratio: float = 0.2) -> tuple:
+    """ Splits dataset into train, validation and test datasets
+
+        Args:
+            dataset (pd.DataFrame, np.array): The dataset of interest.
+            val_ratio (float, optional): The validation split ration. Defaults to 0.2.
+            test_ratio (float, optional): The test split ration. Defaults to 0.2.
+
+        Returns:
+            Returns a tuple containing three objects, the first one is a slice of the
+            dataset containing the training data, the second one contains the validation
+            data and the last one contains the test data. 
+    """
 
     test_m = 1. - test_ratio
     val_m = test_m - val_ratio
